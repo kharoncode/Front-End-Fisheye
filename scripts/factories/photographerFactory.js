f747f4230942a9fa51cc40ce0baf6187e771811c
Remi Collin <sharon.code@proton.me>
2023-05-08 18:42:10 +0200

    add photographer.html id link

--- scripts/factories/photographerFactory.js
+++ scripts/factories/photographerFactory.js
@@ -1,25 +1,47 @@
 export function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+        // Photographer Card
         const article = document.createElement( 'article' );
+        article.classList.add('photographer-card')
+        // Anchor
+        const anchorElement = document.createElement('a');
+        anchorElement.href = `photographer.html?id=${id}`;
+        anchorElement.textContent = "test"
+        // Picture (portrait)
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.src = picture;
+        img.alt = `Portrait de ${name}`;
+        // Name
         const photographerName = document.createElement( 'h2' );
         photographerName.textContent = name;
-        const photographerLocation = document.createElement('h3');
-        photographerLocation.textContent = `${city}, ${country}`
-        const photographerTagline = document.createElement('h4');
-        photographerTagline.textContent = tagline;
-        const photographerPrice = document.createElement('p');
-        photographerPrice.textContent = `${price}€/jour`
         article.appendChild(img);
         article.appendChild(photographerName);
-        article.appendChild(photographerLocation);
-        article.appendChild(photographerTagline);
-        article.appendChild(photographerPrice);
+        article.appendChild(anchorElement);
+
+        // Information
+        const div = document.createElement('div');
+        div.classList.add('photographer-info')
+        article.appendChild(div);
+        // Location
+        const photographerLocation = document.createElement('p');
+        photographerLocation.className = 'photographer-info--location';
+        photographerLocation.textContent = `${city}, ${country}`
+        // Tagline
+        const photographerTagline = document.createElement('p');
+        photographerTagline.className = 'photographer-info--tagline';
+        photographerTagline.textContent = tagline;
+        // Price
+        const photographerPrice = document.createElement('p');
+        photographerPrice.className = 'photographer-info--price';
+        photographerPrice.textContent = `${price}€/jour`
+        div.appendChild(photographerLocation);
+        div.appendChild(photographerTagline);
+        div.appendChild(photographerPrice);
+        
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,12 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+    return await fetch('../data/photographers.json').then(photographers => photographers.json());
+} 
+
+let params = (new URL(document.location)).searchParams;
+let id = parseInt(params.get('id'));
+
+const main = document.querySelector('main');
+const photographerName = document.createElement('p');
+photographerName.textContent = "test";
+main.appendChild(photographerName);
+
